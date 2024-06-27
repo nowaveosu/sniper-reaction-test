2fbc0bc11ce001e833bee81281882d0070d9dc0b
nowaveosu <nowaveosu@gmail.com>
2024-06-27 16:49:41 +0900

    [Add] reaction time 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,21 @@
 
 import Image from "next/image";
 import bot from "../../public/bot.png";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Home() {
   const [showImage, setShowImage] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState<number | null>(null);
+  const startTimeRef = useRef<number>(0);
 
   const handleStart = () => {
     setShowImage(true);
-    setRandomTimeout();
+    startTimeRef.current = Date.now(); 
   };
 
   const handleImageClick = () => {
+    const endTime = Date.now(); 
+    setElapsedTime(endTime - startTimeRef.current); 
     setShowImage(false);
     setRandomTimeout();
   };
@@ -21,11 +25,12 @@ export default function Home() {
     const randomSec = getRandomSec();
     setTimeout(() => {
       setShowImage(true);
+      startTimeRef.current = Date.now();
     }, +randomSec * 1000);
   };
 
   function getRandomSec() {
-    return (Math.random() * 3).toFixed(2);
+    return (Math.random() * 5).toFixed(2) + 5;
   }
 
   return (
@@ -40,9 +45,12 @@ export default function Home() {
         <Image
           src={bot}
           alt="Bot Image"
-          className="w-20 mt-4 cursor-pointer"
+          className="w-90 mt-4 cursor-pointer"
           onClick={handleImageClick}
         />
+      )}
+      {elapsedTime !== null && (
+        <p className="mt-2">reaction Time: {elapsedTime} ms</p>
       )}
     </div>
   );
