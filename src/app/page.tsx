26510c85064ea1dd46f5dfbcd1b073f44a2b791d
nowaveosu <nowaveosu@gmail.com>
2024-06-27 16:01:13 +0900

    [Add] start 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
-
 "use client";
 
 import Image from "next/image";
 import bot from "../../public/bot.png";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [showImage, setShowImage] = useState(false);
 
-  useEffect(() => {
+  const handleStart = () => {
     const randomSec = getRandomSec();
-    const timeoutId = setTimeout(() => { 
+    setTimeout(() => {
       setShowImage(true);
     }, +randomSec * 1000);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
+  };
 
   function getRandomSec() {
-    return (Math.random() * 3).toFixed(2); 
+    return (Math.random() * 3).toFixed(2);
   }
 
   return (
     <div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleStart}>
+        START
+      </button>
       {showImage && (
         <Image
           src={bot}
           alt="Bot Image"
-          className="w-20" 
+          className="w-20 mt-4" // 이미지 위에 margin 추가
         />
       )}
     </div>
