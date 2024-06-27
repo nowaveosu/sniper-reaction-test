d1b6febc37183850fe0f1f22f6632341b41aa8be
nowaveosu <nowaveosu@gmail.com>
2024-06-27 16:04:23 +0900

    [Add] bot을 클릭하면 초 리셋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,16 @@ export default function Home() {
   const [showImage, setShowImage] = useState(false);
 
   const handleStart = () => {
+    setShowImage(true);
+    setRandomTimeout();
+  };
+
+  const handleImageClick = () => {
+    setShowImage(false);
+    setRandomTimeout();
+  };
+
+  const setRandomTimeout = () => {
     const randomSec = getRandomSec();
     setTimeout(() => {
       setShowImage(true);
@@ -20,14 +30,18 @@ export default function Home() {
 
   return (
     <div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleStart}>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleStart}
+      >
         START
       </button>
       {showImage && (
         <Image
           src={bot}
           alt="Bot Image"
-          className="w-20 mt-4" // 이미지 위에 margin 추가
+          className="w-20 mt-4 cursor-pointer"
+          onClick={handleImageClick}
         />
       )}
     </div>
